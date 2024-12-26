897a2bdc97a44b8342967365463dcd3ade0a1dae
Anirban Basak <jeetbasak2002@gmail.com>
2024-12-27 03:58:28 +0530

    refactor - Added Custom Hook for fetching and deleting Todo Data

--- src/page/ShowTodoPage.jsx
+++ src/page/ShowTodoPage.jsx
@@ -1,34 +1,23 @@
-import { useEffect, useState } from "react";
-import { deleteTodo, fetchUserTodo } from "../services/TodoService";
+import { useEffect } from "react";
 import TodoItemComponent from "../components/TodoItem";
+import useFetchTodo from "../hooks/useFetchTodo";
 
 function ShowTodoPage() {
-  // Todo List State
-  const [todoState, setTodoState] = useState([]);
+  // Todo State
+  const { data, fetchTodo, deleteTodoById } = useFetchTodo();
 
-  const fetchData = () => {
-    fetchUserTodo()
-      .then((data) => setTodoState(data))
-      .catch((error) => console.log(`Error Occured : ${error}`));
-  };
-
-  useEffect(() => fetchData(), []);
-
-  // When we delete an todo List
-  const onDelete = (indexToDelete) => {
-    deleteTodo(todoState[indexToDelete].id)
-      .then(() => fetchData())
-      .catch((error) => console.log(error));
-  };
+  useEffect(() => {
+    fetchTodo();
+  }, []);
 
   return (
     <div style={{ flex: 1 }}>
       <div className="todo-list-container">
-        {todoState.map((todo, index) => (
+        {data?.map((todo) => (
           <TodoItemComponent
-            key={index}
+            key={todo.id}
             todo={todo}
-            onDeleteClick={() => onDelete(index)}
+            onDeleteClick={() => deleteTodoById(todo.id)}
           />
         ))}
       </div>
--- src/services/TodoService.js
+++ src/services/TodoService.js
@@ -1,24 +1,6 @@
 const BASE_URL = "http://localhost:8080";
 const TODO_ENDPOINT = "/todos";
 
-export async function fetchUserTodo() {
-  const token = localStorage.getItem("token");
-  return fetch(BASE_URL + TODO_ENDPOINT, {
-    headers: {
-      Authorization: `Bearer ${token}`,
-    },
-  })
-    .then((response) => {
-      if (!response.ok) throw new Error(`${response.statusText}`);
-
-      return response.json();
-    })
-    .catch((error) => {
-      console.log(`Error Occured : ${error.message}`);
-      throw error;
-    });
-}
-
 export async function postTodo(todoData) {
   const token = localStorage.getItem("token");
   return fetch(BASE_URL + TODO_ENDPOINT, {
@@ -38,16 +20,4 @@ export async function postTodo(todoData) {
       console.log(`Error Occured : ${error.message}`);
       throw error;
     });
-}
-
-export async function deleteTodo(todoId) {
-  const token = localStorage.getItem("token");
-  return fetch(BASE_URL + TODO_ENDPOINT + `/${todoId}`, {
-    method: "DELETE",
-    headers: {
-      Authorization: `Bearer ${token}`,
-    },
-  }).then((response) => {
-    if (!response.ok) throw new Error("Error Occured in Deleting TODO");
-  });
 }
