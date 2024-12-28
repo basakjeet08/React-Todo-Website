94cee7abe3ec9e5714257f5d067cbb332fe99416
Anirban Basak <jeetbasak2002@gmail.com>
2024-12-28 20:20:34 +0530

    feat - Added TodoItem component for color changing card

--- src/components/TodoItem.jsx
+++ src/components/TodoItem.jsx
@@ -1,13 +1,17 @@
-import PrimaryButton from "./PrimaryButton";
 import DeleteForever from "@mui/icons-material/Delete";
 
 function TodoItem({ todo, onClick }) {
   return (
-    <div className="flex flex-row items-center gap-4 rounded-lg p-5 bg-secondary transition-all duration-200 hover:shadow-xl w-[350px]">
-      <p className="text-sm font-thin">{todo.description}</p>{" "}
-      <PrimaryButton onClick={onClick}>
+    <div className="group hover:bg-primary flex flex-row items-center justify-between gap-4 rounded-lg p-5 bg-secondary transition-all duration-200 hover:shadow-xl w-[350px]">
+      <p className="text-sm font-thin group-hover:text-text">
+        {todo.description}
+      </p>
+      <button
+        onClick={onClick}
+        className="flex items-center justify-center p-3 border-none bg-primary rounded-md text-white cursor-pointer transition-all duration-300 hover:opacity-80 active:opacity-50 group-hover:bg-secondary group-hover:text-primary"
+      >
         <DeleteForever />
-      </PrimaryButton>
+      </button>
     </div>
   );
 }
