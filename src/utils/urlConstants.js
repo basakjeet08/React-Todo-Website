2dd92d1757549bd6d05702d2a840d5da7cdca733
Anirban Basak <jeetbasak2002@gmail.com>
2024-12-30 04:47:17 +0530

    feat - Environmental variables added for easier updates

--- src/utils/urlConstants.js
+++ src/utils/urlConstants.js
@@ -1,7 +1,6 @@
-export const BASE_URL =
-  "https://miraculous-spontaneity-production.up.railway.app";
+export const BASE_URL = process.env.BASE_URL;
 
 export const VERIFY_ENDPOINT = "/verify";
 export const LOGIN_ENDPOINT = "/login";
 export const REGISTER_ENDPOINT = "/register";
-export const TODO_ENDPOINT = "/todos"
+export const TODO_ENDPOINT = "/todos";
