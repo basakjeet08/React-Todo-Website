2302c08d8ce201109378121cba30f1bfdfa7ec5c
Anirban Basak <jeetbasak2002@gmail.com>
2025-01-20 12:58:09 +0530

    refactor - removed the deployment code since we are taking down the deployed servers.

--- src/utils/urlConstants.js
+++ src/utils/urlConstants.js
@@ -1,5 +1,4 @@
-export const BASE_URL =
-  "https://miraculous-spontaneity-production.up.railway.app";
+export const BASE_URL = "http://localhost:8080";
 
 export const VERIFY_ENDPOINT = "/verify";
 export const LOGIN_ENDPOINT = "/login";
